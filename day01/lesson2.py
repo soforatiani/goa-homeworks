393c6bdda6b79627fed8630f82c1f06320b5917c
soforatiani <ratianisopo88@gmail.com>
2024-10-06 20:58:28 +0400

    commit

---
+++ day01/lesson2.py
@@ -0,0 +1,9 @@
+name="my name is sofo"
+surname="ratiani"
+age= 22
+height=171.5
+knows_pharmacy=True
+knows_programming=False
+goal="my goal is to be good student"
+connector="my age and height is"
+print(name+" " +surname+" "+connector+" " +str(age)+" "+str(height)+" "+(goal))
