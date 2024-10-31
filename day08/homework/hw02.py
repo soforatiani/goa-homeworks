1228cce01897575d1e582ca9608f1e0198b4a6b0
soforatiani <ratianisopo88@gmail.com>
2024-10-31 17:27:14 +0400

    commit

---
+++ day08/homework/hw02.py
@@ -0,0 +1,12 @@
+#gaixsenet monacemta tipebi da sheasworet es kodi
+ 
+#value="10"
+#result=value*5
+#print("result")
+
+
+
+
+value=10
+result=10*5
+print(result)
---
+++ day08/homework/hw05.py
@@ -0,0 +1,14 @@
+# num1=6
+# num2=3
+# num3=10
+
+# average=num1+num2+num3/3
+# print(average)
+
+
+
+num1=6
+num2=3
+num3=10
+average=(num1+num2+num3)/3
+print(average)
