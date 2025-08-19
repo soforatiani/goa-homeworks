9e6337df7ccfaf61e8b894b6477f3dd176b27619
soforatiani <ratianisopo88@gmail.com>
2025-08-20 00:40:42 +0400

    commit

---
+++ newfolder/nw/cw.py
@@ -0,0 +1,11 @@
+for i in range (0,51):
+	if i%3==0 and i%5==0:
+		print((str(i))+"FIZZBUZZ" )
+	elif i%3==0:
+		print((str(i))+"FIZZ" )
+	elif i%5==0:
+		print	((str(i))+"BUZZ" )
+	
+	else:
+		print(i)	
+
